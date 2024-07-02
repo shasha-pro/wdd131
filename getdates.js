3b35650335debc3cfa0f8ec60758a6292dd23546
Patrick S. Dunoh <patricksiafa@gmail.com>
2024-07-02 08:46:58 +0000

    bfdfldf

---
+++ getdates.js
@@ -0,0 +1,7 @@
+const currentYear = document.querySelector("#currentyear");
+const year = new Date().getFullYear();
+
+const lastModfiedDate = document.querySelector("#lastModified");
+currentYear.innerHTML = year;
+
+lastModfiedDate.innerHTML = new Date(document.lastModified);
